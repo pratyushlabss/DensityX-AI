95d4284bf6cfcf243c155b2b4d177e8adaa68d3d
Gayathri Sathish <gayathrisathish@192.168.1.27>
2026-03-03 17:54:55 +0530

    backend stable and working locally

--- backend/main.py
+++ backend/main.py
@@ -15,6 +15,7 @@ import uvicorn
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
